5c7ff6522a21ada826a1b66885bfa27ca0a21e75
Lukas Marek <lukas.marek@gmail.com>
2011-07-24 17:59:04 +0200

    - little more

---
+++ Birthday-device-test/src/cz/krtinec/birthday/PhotoLoaderTest.java
@@ -0,0 +1,42 @@
+/*
+ * This file is part of Birthday Widget.
+ *
+ * Birthday Widget is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Birthday Widget is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Birthday Widget.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Copyright (c) Lukas Marek, 2011.
+ */
+
+package cz.krtinec.birthday;
+
+import android.content.Context;
+import android.os.Handler;
+import android.widget.ImageView;
+import cz.krtinec.birthday.ui.PhotoLoader;
+import junit.framework.TestCase;
+import static org.mockito.Mockito.*;
+
+/**
+ * User: lukas.marek@cleverlance.com
+ * Time: 17:15
+ */
+public class PhotoLoaderTest extends TestCase {
+    public void testPhotoLoader() {
+        Handler handler = mock(Handler.class);
+        Context ctx = mock(Context.class);
+        PhotoLoader loader = new PhotoLoader(handler, ctx);
+        new Thread(loader).start();
+        ImageView iv = mock(ImageView.class);
+        loader.addPhotoToLoad(iv, 123l);
+    }
+}
