6133a32622858e6ea0e0151ae435c62018178b08
Lukas Marek <lukas.marek@gmail.com>
2011-07-24 19:55:58 +0200

    - Smooth scrolling in scroll view

--- Birthday/src/cz/krtinec/birthday/ui/PhotoLoader.java
+++ Birthday/src/cz/krtinec/birthday/ui/PhotoLoader.java
@@ -21,10 +21,12 @@ package cz.krtinec.birthday.ui;
 
 import java.io.InputStream;
 
+import java.util.HashMap;
 import java.util.Map;
 import java.util.WeakHashMap;
 import java.util.concurrent.ConcurrentHashMap;
 
+import android.util.Log;
 import cz.krtinec.birthday.R;
 import cz.krtinec.birthday.data.BirthdayProvider;
 import android.content.Context;
@@ -36,50 +38,78 @@ public class PhotoLoader implements Runnable {
     private final Handler handler;
     private final Context ctx;
     private Map<ImageView, Long> photosToLoad = new ConcurrentHashMap<ImageView, Long>();
+    private static Drawable DEFAULT_PHOTO;
 
     private boolean shutdown = false;
-    private Map<Long, Drawable> cache = new WeakHashMap<Long, Drawable>();
-    
+    private boolean paused = false;
+
+    private Map<Long, Drawable> cache = new ConcurrentHashMap<Long, Drawable>();
+
     public PhotoLoader(Handler handler, Context ctx) {
-            this.handler = handler;
-            this.ctx = ctx;
+        this.handler = handler;
+        this.ctx = ctx;
+        DEFAULT_PHOTO = ctx.getResources().getDrawable(R.drawable.icon);
     }
 
-    public void addPhotoToLoad(ImageView icon, long contactId) {        	
-        photosToLoad.put(icon, contactId);
+    public void addPhotoToLoad(ImageView icon, long contactId) {
+        Drawable d;
+        if ((d = cache.get(contactId)) != null) {
+            Log.v("PhotoLoader", "cache hit for: " + contactId);
+            icon.setImageDrawable(d);
+        } else {
+            Log.v("PhotoLoader", "cache miss for: " + contactId);
+            photosToLoad.put(icon, contactId);
+        }
     }
-    
+
     public void shutdown() {
-    	this.shutdown = true;
+        this.cache.clear();
+        this.shutdown = true;
     }
-    
+
+    public void pause() {
+        this.paused = true;
+    }
+
+    public void resume() {
+        this.paused = false;
+    }
+
+    private void updateImage(final ImageView icon, final Drawable contactPhoto) {
+        handler.post(new Runnable() {
+            public void run() {
+                icon.setImageDrawable(contactPhoto);
+            }
+        });
+
+    }
+
     @Override
     public void run() {
-    		while (!shutdown) {
+        while (!shutdown) {
+            if (paused) {
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                    //do nothing
+                }
+            } else {
                 if (!photosToLoad.isEmpty()) {
-                    for (final ImageView icon: photosToLoad.keySet()) {
+                    for (final ImageView icon : photosToLoad.keySet()) {
                         Long contactId = photosToLoad.get(icon);
                         Drawable drawable = null;
-                        drawable = cache.get(contactId);
-                        if (drawable == null) {
-                            InputStream photoStream;
-                            if ((photoStream = BirthdayProvider.openPhoto(ctx, contactId)) != null) {
-                                drawable = Drawable.createFromStream(photoStream, "src");
-                                cache.put(contactId, drawable);
-                            }
+                        InputStream photoStream;
+                        if ((photoStream = BirthdayProvider.openPhoto(ctx, contactId)) != null) {
+                            drawable = Drawable.createFromStream(photoStream, "src");
+                        } else {
+                            drawable = DEFAULT_PHOTO;
                         }
-                        final Drawable drawable1 = drawable;
-                        handler.post(new Runnable() {
-                            public void run() {
-                                if (drawable1 != null) {
-                                    icon.setImageDrawable(drawable1);
-                                } else {
-                                    icon.setImageResource(R.drawable.icon);
-                                }
-                            }
-                        });
+                        cache.put(contactId, drawable);
+                        photosToLoad.remove(icon);
+                        updateImage(icon, drawable);
                     }
                 }
-    		}
+            }
+        }
     }
 }
