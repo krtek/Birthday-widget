563e5782ed78f3e65e9470633bbfda8361c4e655
Lukas Marek <lukas.marek@gmail.com>
2011-07-26 22:52:10 +0200

    - Reformat

--- Birthday/src/cz/krtinec/birthday/BirthdayDebug.java
+++ Birthday/src/cz/krtinec/birthday/BirthdayDebug.java
@@ -39,78 +39,80 @@ import android.widget.ListView;
 import android.widget.TextView;
 
 public class BirthdayDebug extends Activity {
-	private PhotoLoader loader;
-	   /** Called when the activity is first created. */
+    private PhotoLoader loader;
+
+    /**
+     * Called when the activity is first created.
+     */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.debug);
     }
-    
-	@Override
-	protected void onResume() {		
-		super.onResume();
-		 ListView list = (ListView) findViewById(R.id.debug_list);		 	        
-			loader = new PhotoLoader(new Handler(), this);
-			Thread thread = new Thread(loader);
-			thread.setPriority(Thread.MIN_PRIORITY);
-			thread.start();
-	        List<EventDebug> listOfContacts = BirthdayProvider.getInstance().allBirthday(this);
-	        list.setAdapter(new BirthdayDebugAdapter(listOfContacts, this, loader));    
-	}
-	
-	@Override
-	protected void onStop() {
-		super.onStop();
-		loader.shutdown();
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        ListView list = (ListView) findViewById(R.id.debug_list);
+        loader = new PhotoLoader(new Handler(), this);
+        Thread thread = new Thread(loader);
+        thread.setPriority(Thread.MIN_PRIORITY);
+        thread.start();
+        List<EventDebug> listOfContacts = BirthdayProvider.getInstance().allBirthday(this);
+        list.setAdapter(new BirthdayDebugAdapter(listOfContacts, this, loader));
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        loader.shutdown();
 //		Debug.stopMethodTracing();		
-	}
-	
+    }
+
     static class BirthdayDebugAdapter extends AdapterParent<EventDebug> {
-    	public BirthdayDebugAdapter(List<EventDebug> list, Context ctx, PhotoLoader loader) {
-    		super(list, ctx, loader);
-    	}
+        public BirthdayDebugAdapter(List<EventDebug> list, Context ctx, PhotoLoader loader) {
+            super(list, ctx, loader);
+        }
 
-		@Override
-		public View getView(int position, View convertView, ViewGroup parent) {
-			EventDebug contact = list.get(position);
-			View v;
-			if (convertView == null) {
-				LayoutInflater vi = (LayoutInflater)ctx.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-				v = vi.inflate(R.layout.debug_row, null);			
-			} else {
-				v = convertView;
-			}
-			
-			((TextView)v.findViewById(R.id.name)).setText(contact.getDisplayName());				
-			((TextView)v.findViewById(R.id.dateParsed)).setText(MessageFormat.format(ctx.getString(R.string.debug_parsed), contact.getDisplayDate(ctx)));
-			
-			((TextView)v.findViewById(R.id.dateAsString)).setText(MessageFormat.format(ctx.getString(R.string.debug_raw), contact.getbDayString()));				
-			ImageView check = (ImageView)v.findViewById(R.id.check);
-			
-			switch (contact.getIntegrity()) {
-				case FULL: {
-					check.setImageResource(R.drawable.accept);
-					break;
-				}
-				case WITHOUT_YEAR: {
-					check.setImageResource(R.drawable.exclamation);
-					break;					
-				}
-				case NONE: {
-					check.setImageResource(R.drawable.cancel);
-					break;										
-				}
-			}
-			final ImageView imageView = (ImageView)v.findViewById(R.id.bicon);
-			imageView.setImageResource(R.drawable.icon);			
-			loader.addPhotoToLoad((ImageView)v.findViewById(R.id.bicon), contact.getId());
-			
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            EventDebug contact = list.get(position);
+            View v;
+            if (convertView == null) {
+                LayoutInflater vi = (LayoutInflater) ctx.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                v = vi.inflate(R.layout.debug_row, null);
+            } else {
+                v = convertView;
+            }
 
-			
-			return v;
+            ((TextView) v.findViewById(R.id.name)).setText(contact.getDisplayName());
+            ((TextView) v.findViewById(R.id.dateParsed)).setText(MessageFormat.format(ctx.getString(R.string.debug_parsed), contact.getDisplayDate(ctx)));
 
-		}    	
+            ((TextView) v.findViewById(R.id.dateAsString)).setText(MessageFormat.format(ctx.getString(R.string.debug_raw), contact.getbDayString()));
+            ImageView check = (ImageView) v.findViewById(R.id.check);
+
+            switch (contact.getIntegrity()) {
+                case FULL: {
+                    check.setImageResource(R.drawable.accept);
+                    break;
+                }
+                case WITHOUT_YEAR: {
+                    check.setImageResource(R.drawable.exclamation);
+                    break;
+                }
+                case NONE: {
+                    check.setImageResource(R.drawable.cancel);
+                    break;
+                }
+            }
+            final ImageView imageView = (ImageView) v.findViewById(R.id.bicon);
+            imageView.setImageResource(R.drawable.icon);
+            loader.addPhotoToLoad((ImageView) v.findViewById(R.id.bicon), contact.getId());
+
+
+            return v;
+
+        }
     }
 
 }
