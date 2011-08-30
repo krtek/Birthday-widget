72c18540cbcc5a9b15a575e25b239ad865680a18
Lukas Marek <lukas.marek@gmail.com>
2011-08-30 14:55:24 +0200

    Updated license.

--- Birthday/src/cz/krtinec/birthday/ui/StockPhotoLoader.java
+++ Birthday/src/cz/krtinec/birthday/ui/StockPhotoLoader.java
@@ -12,6 +12,8 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ *
+ * Modified by Lukas Marek (lukas.marek@gmail.com) for BirthdayWidget.
  */
 
 package cz.krtinec.birthday.ui;
