ceb6eadd4c02053255d3a7548b62012b584df848
Lukáš Marek <lukas.marek@fragaria.cz>
2013-12-05 15:44:39 +0100

    Created .gitignore

---
+++ app/build/source/buildConfig/debug/cz/krtinec/birthday/BuildConfig.java
@@ -0,0 +1,7 @@
+/** Automatically generated file. DO NOT MODIFY */
+package cz.krtinec.birthday;
+
+public final class BuildConfig {
+    public static final boolean DEBUG = Boolean.parseBoolean("true");
+
+}
